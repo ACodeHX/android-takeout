ffa2833b332085c591528efdc4364b28aa87d893
ACodeHX <1468232850@qq.com>
2024-05-26 13:42:47 +0800

    商店的列表初步实现

--- app/src/main/java/com/aa/meituan/Store.java
+++ app/src/main/java/com/aa/meituan/Store.java
@@ -8,15 +8,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Store extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private StoreAdapter myAdapter;
+    private List<StoreMinute> itemList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        itemList = new ArrayList<>();
+
+        itemList.add(new StoreMinute("title 1", "sub", R.drawable.must_buy_two));
+
+        myAdapter = new StoreAdapter(itemList);
+        recyclerView.setAdapter(myAdapter);
     }
 }
---
+++ app/src/main/java/com/aa/meituan/StoreAdapter.java
@@ -0,0 +1,52 @@
+package com.aa.meituan;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder> {
+    private List<StoreMinute> itemList;
+
+    public StoreAdapter(List<StoreMinute> itemList) {
+        this.itemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.store, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        StoreMinute item = itemList.get(position);
+        holder.title.setText(item.getTitle());
+        holder.subtitle.setText(item.getSubtitle());
+        holder.image.setImageResource(item.getImageResourceId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public ImageView image;
+        public TextView title;
+        public TextView subtitle;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.item_image);
+            title = itemView.findViewById(R.id.item_title);
+            subtitle = itemView.findViewById(R.id.item_subtitle);
+        }
+    }
+}
