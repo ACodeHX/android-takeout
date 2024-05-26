d673f48bec579a8144adfec0483fc3e35d4a37df
ACodeHX <1468232850@qq.com>
2024-05-26 10:58:22 +0800

    添加布局文件和字段的文件将SDK变为34

--- app/src/main/java/com/aa/meituan/MainActivity.java
+++ app/src/main/java/com/aa/meituan/MainActivity.java
@@ -3,12 +3,23 @@ package com.aa.meituan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userET;
+    private EditText passET;
+    private Button loginBUT;
+    private Button regBUT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userET = findViewById(R.id.user);           //用户
+        passET = findViewById(R.id.passwd);         //密码
+        loginBUT = findViewById(R.id.login_but);    //登陆按钮
+        regBUT = findViewById(R.id.register);       //注册按钮
     }
 }
---
+++ app/src/main/java/com/aa/meituan/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.aa.meituan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+}
