e00d51e826b8066d13ea7ba7365d0f3a3c635a92
ACodeHX <1468232850@qq.com>
2024-06-03 16:42:06 +0800

    新增:详细商品,图片
    修改:
    删除:
    完成:
    下一个目标:

--- app/src/main/java/com/aa/meituan/TakeOut.java
+++ app/src/main/java/com/aa/meituan/TakeOut.java
@@ -1,11 +1,16 @@
 package com.aa.meituan;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -28,9 +34,11 @@ public class TakeOut extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ImageView backStore;
     private Button judgePay;
+    private Button payMinute;
     private double totalPrice = 0.0; // 设置总价
     private List<TakeOutValue> takeOutValueList;
     private List<TakeOutValue> filteredList;
+    private PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,7 +92,14 @@ public class TakeOut extends AppCompatActivity {
         // 更新适配器数据
         adapter.notifyDataSetChanged();
 
-        //是指图片
+        //购物详细
+        payMinute = findViewById(R.id.select_store);
+        payMinute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBottomSheetDialog();
+            }
+        });
 
         //跳转支付界面
         judgePay = findViewById(R.id.payFood);
@@ -115,6 +130,14 @@ public class TakeOut extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void showBottomSheetDialog() {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        View bottomSheetView = LayoutInflater.from(this).inflate(R.layout.shoppinglist, null);
+
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
     }
 
     private void loadMealsFromJson(String storeId) {
